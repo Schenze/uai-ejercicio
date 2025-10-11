4ca1365c7819ace704df0e56db0ecfa0f3c5e168
Schenze <franquiturri01@gmail.com>
2025-10-10 23:29:39 -0300

    deleted:    Ejercicio1.html 	new file:   ejercicio/exercises/Arrays.js 	new file:   ejercicio/exercises/strings.js 	new file:   ejercicio/exercises/variables_and_operators.js 	new file:   ejercicio/index.html

---
+++ ejercicio/exercises/strings.js
@@ -0,0 +1,25 @@
+var stringUpper;
+stringUpper = "texto en minuscula";
+String.prototype.toUpperCase(stringUpper);
+console.log(stringUpper);
+
+var stringSub,stringmitad1,stringmitad2;
+stringSub = "Hola mundo";
+stringmitad1 = stringSub.substring(0,4);
+stringmitad2 = stringSub.substring(6,10);
+console.log(stringmitad1 + "\n");
+console.log(stringmitad2);
+
+var stringconv,stringconv2;
+stringconv = "buenos dias";
+stringconv2 = stringconv;
+stringconv = stringconv.substring(0,1);
+stringconv2 = stringconv2.substring(1,11);
+String.prototype.toUpperCase(stringconv);
+console.log(stringconv + stringconv2);
+
+var stringIndex,indice;
+stringIndex = "buenas tardes";
+indice = stringIndex.indexOf(" ");
+console.log("Ubicacion del indice: ",indice);
+
---
+++ ejercicio/exercises/variables_and_operators.js
@@ -0,0 +1,12 @@
+var primernumero,segundonumero,resultado;
+primernumero = 5;
+segundonumero = 5;
+resultado = primernumero + segundonumero;
+console.log("resultado");
+
+var texto1,texto2,sumadetexto;
+texto1 = "Hello "; //espacio para que el texto no este pegado
+texto2 = "Worlds!!!";
+sumadetexto = texto1 + texto2;
+console.log("sumadetexto");
+
