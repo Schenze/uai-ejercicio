8aab54ffa518df6e1d8892a2ffe20760858bbb5f
Schenze <franquiturri01@gmail.com>
2025-10-11 22:45:27 -0300

    modified:   ejercicio/exercises/Arrays.js 	modified:   index.html

--- ejercicio/exercises/Arrays.js
+++ ejercicio/exercises/Arrays.js
@@ -1,1 +1,21 @@
 //completar
+var arrayMeses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre"];
+console.log(arrayMeses[4],arrayMeses[10]);
+
+console.log(arrayMeses.sort());
+
+arrayMeses.unshift(Banana);
+arrayMeses.push(Manzana);
+console.log(arrayMeses);
+
+var elementoPrimero = arrayMeses.shift();
+arrayMeses.pop();
+console.log(arrayMeses);
+
+var arrayReverse
+arrayReverse = arrayMeses.reverse();
+console.log(arrayReverse);
+
+console.log(arrayMeses.join("-"));
+
+
